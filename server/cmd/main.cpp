a524400ddacaf2a56252fbd91f4dae2762b908ae
Sarah_deep <91503476+Sarahdeep@users.noreply.github.com>
2023-05-01 11:03:01 +0300

    Сделал классы сущностей, написал интерфейсы для взаимодействия с бд, и одну реализацию

---
+++ server/cmd/main.cpp
@@ -0,0 +1,11 @@
+
+
+#include "../internal/users/include/UserRepository.hpp"
+
+int main(){
+    User user{"qwerty200468@gmail.com", "123", "tolik"};
+    conn conn;
+    UserRepository repo(conn);
+    std::cout<<conn.getData();
+    repo.makeUser(user);
+}

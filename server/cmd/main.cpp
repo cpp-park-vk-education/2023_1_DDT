d0daca0415946f5baaf630aa84af51df7f6a9a23
marcheanin <marcheanin1576@gmail.com>
2023-05-16 02:48:00 +0300

    add LevDistTokenMetric impl

--- server/cmd/main.cpp
+++ server/cmd/main.cpp
@@ -2,22 +2,36 @@
 
 #include "PythonAntlr.h"
 #include "MyCppAntlr.h"
+#include "TokenMetricLib.h"
 
 int main(int argc, const char* argv[]) {
   //   ifstream ins("/home/denis/2023_1_DDT/antlr/test.py");
-  std::ifstream ins(
-      "/home/denis/2023_1_DDT/server/pkg/antlr/testprogs/cpp/test.cpp");
+  std::ifstream fin1("internal/metrics/testProgs/cpp/code1.txt");
+  std::ifstream fin2("internal/metrics/testProgs/cpp/code2.txt");
 
-  MyCppAntlr pA = MyCppAntlr(ins);
+  MyCppAntlr cppA1 = MyCppAntlr(fin1);
+  MyCppAntlr cppA2 = MyCppAntlr(fin2);
 
-  std::vector<int> tokens = pA.getTokensTypes();
+  std::vector<int> tokens1 = cppA1.getTokensTypes();
+  std::vector<int> tokens2 = cppA2.getTokensTypes();
 
-  std::cout << "Tokens:" << std::endl;
-  for (int token : tokens) {
-    std::cout << token << std::endl;
+  LevDistTokenMetric lev;
+  lev.setData(tokens1, tokens2);
+
+  std::cout << "Tokens1:" << std::endl;
+  for (int token : tokens1) {
+    std::cout << token << " ";
   }
+  std::cout << std::endl;
 
-  // std::cout << pA.getTreeString() << std::endl;
+  std::cout << "Tokens2:" << std::endl;
+  for (int token : tokens2) {
+      std::cout << token << " ";
+  }
+  std::cout << std::endl;
+
+
+  std::cout << lev.getMetric() << std::endl;
 
   return 0;
 }
--- server/internal/metrics/src/TokenMetricImpl.cpp
+++ server/internal/metrics/src/TokenMetricImpl.cpp
@@ -4,12 +4,39 @@
 
 #include "TokenMetricLib.h"
 
-void PrepareDataTokenMetric::setData(std::vector<std::string> _tokens1, std::vector<std::string> _tokens2) {}
+double LevDistTokenMetric::getMetric() {
+    unsigned long n = tokens1.size();
+    unsigned long m = tokens2.size();
+    int x, y, z;
 
-double PrepareDataTokenMetric::getMetric() {
-    return metric_res;
+    std::vector <std::vector <int> > lev (n, std::vector <int> (m, 0));
+
+    for (size_t i = 0; i < n; i++){
+        for (size_t j = 0; j < m; j++){
+            if (std::min(i, j) == 0){
+                lev[i][j] = static_cast <int> (std::max(i, j));
+            }
+            else{
+                x = lev[i-1][j];
+                y = lev[i][j-1];
+                z = lev[i-1][j-1];
+                lev[i][j] = std::min(x, std::min(y, z));
+                if (tokens1[i] != tokens2[j]){
+                    lev[i][j]++;
+                }
+            }
+        }
+    }
+
+    if (n == 0 || m == 0)
+        return 0;
+    double res = 1.0 - static_cast<double> (lev[n-1][m-1]) / static_cast<double> (std::max(n ,m));
+    return  res;
 }
 
-void LivDistTokenMetric::countMetric() {}
+double WShinglingTokenMetric::getMetric() {}
 
-void WShinglingTokenMetric::countMetric() {}
+void PrepareDataTokenMetric::setData(std::vector<int> _tokens1, std::vector<int> _tokens2) {
+    tokens1 = _tokens1;
+    tokens2 = _tokens2;
+}
